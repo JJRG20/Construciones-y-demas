c85f2ef56ce216622953fd684e6dfdf4503df30d
JJRG20 <juanjo_resk@live.com>
2023-05-29 10:14:43 -0400

    Nuevo ingreso de datos

---
+++ formPHP/ingresa1.php
@@ -0,0 +1,27 @@
+<?php
+include("conexion1.php");
+$conexion=conectar();
+
+$num=$_POST['Num'];
+$nombres=$_POST['Nombres'];
+$apellidos=$_POST['Apellidos'];
+$usuario=$_POST['Nombre_us'];
+$password=$_POST['Password'];
+$email=$_POST['Email'];
+$fono=$_POST['Fono'];
+$direccion=$_POST['Direccion'];
+$fecha=$_POST['Fecha_nac'];
+$sexo=$_POST['Sexo'];
+$marca=$_POST['Marca_fav'];
+
+
+$sql="INSERT INTO usuarios1 VALUES('$num','$nombres','$apellidos','$usuario','$password','$email','$fono','$direccion','$fecha','$sexo','$marca')";
+$query= mysqli_query($conexion,$sql);
+
+if($query){
+    Header("Location: usuario1.php");
+    
+}
+else {
+}
+?>

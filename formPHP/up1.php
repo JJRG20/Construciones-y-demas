23e54e3b6db8c33d48743d0e44b133546d1def42
JJRG20 <juanjo_resk@live.com>
2023-05-29 10:19:32 -0400

    Nueva forma de actualizar

---
+++ formPHP/up1.php
@@ -0,0 +1,26 @@
+<?php
+
+include("conexion1.php");
+$conexion=conectar();
+
+$num=$_POST['Num'];
+$nombres=$_POST['Nombres'];
+$apellidos=$_POST['Apellidos'];
+$usuario=$_POST['Nombre_us'];
+$password=$_POST['Password'];
+$email=$_POST['Email'];
+$fono=$_POST['Fono'];
+$direccion=$_POST['Direccion'];
+$fecha=$_POST['Fecha_nac'];
+$sexo=$_POST['Sexo'];
+$marca=$_POST['Marca_fav'];
+
+
+
+$sql="UPDATE usuarios1 SET Nombres='$nombres',Apellidos='$apellidos',Nombre_us='$usuario',Password='$password',Email='$email',Fono='$fono',Direccion='$direccion',Fecha_nac='$fecha',Sexo='$sexo',Marca_fav='$marca' WHERE Num='$num'";
+$query=mysqli_query($conexion,$sql);
+
+    if($query){
+        Header("Location: act_bor.php");
+    }
+?>

f395020880b3136847f743b50a75786718430f88
JJRG20 <juanjo_resk@live.com>
2023-05-29 12:25:44 -0400

    Cambio de posición de los archivos

--- index.php
+++ index.php
@@ -17,255 +17,11 @@ $row=mysqli_fetch_array($query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Construcciones y demás</title>
     <link rel="shortcut icon" href="Img_página/Brick.png">
+    <link rel="stylesheet" href="sa55.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap-grid.css">
     <link rel="stylesheet" href="https://unpkg.com/transition-style">
-    <style>
-        .body{
-            background-color: yellow;
-            
-        }
-        nav{
-            
-            width: 100%;
-            background-color: gold;
-            border-bottom: red 2px solid;
-            font-family: Arial, Helvetica, sans-serif; 
-            padding: 10px;
-            
-            
-        }
-        .car img{
-            width: 100%;
-            height: auto;
-            
-        }
-        .yellow{
-            background-color: greenyellow;
-            font-size: 75px;
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        }
-        .blanco{
-            text-decoration: none;
-            background-color: red;
-            border-radius: 8px;
-            margin-right: 10px;
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        }
-        .blanco:hover{
-            background-color: white;
-        }
-        .anuncionos{
-            background-color: white;
-            height: 520px;
-        }
-
-        .centro{
-            display: grid;
-            grid-template-columns: auto;
-        }
-        .centro div{
-            margin-left: auto;
-            margin-right: auto;
-        }
-        #centro2{
-            width: 80%;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        #centro2 div{
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .ri{
-            margin-left: auto;
-        }
-        .le{
-            margin-right: auto;
-        }
-        .ce{
-            margin-left: auto;
-            margin-right: auto;
-        }
-        h1{
-            font-size: 300%;
-            font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-        }
-        h3{
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        }
-        .intro{
-            
-            text-align: center;
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        }
-        #muestra{
-            width: 300px;
-        }
-        .pie{
-            display: grid;
-            grid-template-columns: 40% 20% 40%;
-            
-        }
-        .pie div{
-            
-            margin-top: 30px;
-        
-        }
-        #for1{
-            display: grid;
-            grid-template-columns: auto auto;
-            width: 420px;
-            height: 225px;
-            background-color: gold;
-            border: red 2px solid;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        #for1 div{
-            padding: 5px;
-        }
-        .datos{
-            font-size: 150%;
-        }
-        #for2{
-            display: grid;
-            grid-template-columns: auto auto;
-            width: 400px;
-            height: 535px;
-            background-color: gold;
-            border: red 2px solid;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        #for2 div{
-            padding: 5px;
-        }
-        form{
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        .nosotros{
-            text-align: justify;
-            padding: 10px;
-            background-color: orange;
-            border: red 2px solid;
-            
-            height: max-content;
-            
-            font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
-        }
-        .caja{
-            display: grid;
-            grid-template-columns: 15% auto 15%;
-            margin-right: auto;
-            margin-left: auto;
-        }
-
-        .fight{
-            padding-left: 10px;
-        }
-
-        .accion{
-            width: 100%;
-
-        }
-        .anuncio1{
-            padding: 0%;
-            width: 200px;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .anuncio1 div{
-            padding: 0%;
-        }
-        .anuncio3{
-            padding: 0%;
-            width: 90%;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .anuncio3 div{
-            padding: 0%;
-        }
-        .pad0{
-            padding: 0%;
-        }
-        iframe{
-            height: 250px; 
-            width: 365px;
-        }
-        .titulo{
-            display: grid;
-            grid-template-columns: 100px auto ;
-        }
-        .footer{
-            background-color: gold;
-            width: 100%;
-            height: 175px;
-            position: absolute;
-            border-top: red 2px solid;
-            font-family: 'Times New Roman', Times, serif; 
-            padding-top: 10px;
-        
-        }
-        .accion2{
-            height: 175px;
-        }
-        .accion2:hover{
-            transform: scale(1.5);
-            
-        }
-        #otro{
-            display: grid;
-            grid-template-columns:auto auto auto;
-            width: 100%;
-            padding: 10px;
-            background-color: greenyellow;
-            border: red 2px solid;
-            margin-left: auto;
-            margin-right: auto;
-        
-        }
-        #otro div{
-            color: red;
-            width: 100%;
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-            font-size: 110%;
-            text-align: center;
-        }
-        .black{
-            color: black;
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        }
-        .bb{
-            width: 110px;
-            height: 40px;
-            text-decoration: none;
-            color: white;
-        }
-        .bb:hover{
-            background-color: red;
-        }
-        .gradient{
-            background: linear-gradient(-45deg,yellow,orange,red);
-            background-size: 400% 400%;
-            animation: gradient 10s ease infinite;
-            box-shadow: 0px 0px 20px black;
-
-        }
-        @keyframes gradient{
-            0%{
-                background-position: 0% 50%;
-            }
-            50%{
-                background-position: 100% 50%;
-            }
-            100%{
-                background-position: 0% 50%;
-            }
-        }
-    </style>
+    
 </head>
 <body class="body">
     <div class="sticky-top">
@@ -276,10 +32,10 @@ $row=mysqli_fetch_array($query);
                         |||
                     </button>
                     <ul class="dropdown-menu bg-warning" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item blanco" href="http://localhost/Consdemas/formPHP/index.php">Página principal</a></li>
+                        <li><a class="dropdown-item blanco" href="http://localhost/Consdemas/index.php">Página principal</a></li>
                         <li><a class="dropdown-item blanco" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Acceder</a></li>
-                        <li><a class="dropdown-item blanco" href="http://localhost/Consdemas/formPHP/usuario1.php">Registrarse</a></li>
-                        <li><a class="dropdown-item blanco" href="http://localhost/Consdemas/formPHP/nosotros.php">Nosotros</a></li>
+                        <li><a class="dropdown-item blanco" href="http://localhost/Consdemas/usuario1.php">Registrarse</a></li>
+                        <li><a class="dropdown-item blanco" href="http://localhost/Consdemas/nosotros.php">Nosotros</a></li>
                     </ul>
                 </div>
                 <div class="container-fluid">
@@ -287,13 +43,13 @@ $row=mysqli_fetch_array($query);
                     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link blanco" aria-current="page" href="http://localhost/Consdemas/formPHP/index.php">Página principal</a>
+                                <a class="nav-link blanco" aria-current="page" href="http://localhost/Consdemas/index.php">Página principal</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link blanco" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Acceder</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link blanco" href="http://localhost/Consdemas/formPHP/nosotros.php">Nosotros</a>
+                                <a class="nav-link blanco" href="http://localhost/Consdemas/nosotros.php">Nosotros</a>
                             </li>
                         </ul>
                     </div>
@@ -305,7 +61,7 @@ $row=mysqli_fetch_array($query);
     <br>
 
     <div class="titulo">
-        <a class="p-2" title="Construcciones y demás" href="http://localhost/Consdemas/formPHP/index.php">
+        <a class="p-2" title="Construcciones y demás" href="http://localhost/Consdemas/index.php">
             <img src="Img_página/Brick.png" alt="Construcciones y demás"></a>
             <div transition-style="in:wipe:left">
                 <h1 class="p-4"><b>Construcciones y demás</b></h1>
@@ -343,7 +99,7 @@ $row=mysqli_fetch_array($query);
                             </div>
                             
                             <button type="submit" class="bb btn btn-danger justify-content-end">Login</button>
-                            <a href="http://localhost/Consdemas/formPHP/usuario1.php" class="bb btn btn-danger justify-content-end">Registrarse</a>
+                            <a href="http://localhost/Consdemas/usuario1.php" class="bb btn btn-danger justify-content-end">Registrarse</a>
                             
                         </div>
                         
@@ -362,7 +118,7 @@ $row=mysqli_fetch_array($query);
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <a href="http://localhost/Consdemas/formPHP/usuario1.php" ><img src="Img_página/únete.jpg" class="d-block w-80" alt="Regístrate"></a> 
+                    <a href="http://localhost/Consdemas/usuario1.php" ><img src="Img_página/únete.jpg" class="d-block w-80" alt="Regístrate"></a> 
                 </div>
                 <div class="carousel-item">
                     <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"><img src="Img_página/log.jpg" class="d-block w-80" alt="Login"></a>
@@ -404,7 +160,7 @@ $row=mysqli_fetch_array($query);
     
     <div class="container car">
         <div>
-            <a href="http://localhost/Consdemas/formPHP/nosotros.php" ><img src="Img_página/Nos.jpg" alt="Esto somos nosotros"></a>   
+            <a href="http://localhost/Consdemas/nosotros.php" ><img src="Img_página/Nos.jpg" alt="Esto somos nosotros"></a>   
         </div>
                  
     </div><br><br>

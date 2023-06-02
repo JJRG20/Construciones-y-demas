2601ae5c2880e42b6369f117109baee0134e8dd3
JJRG20 <juanjo_resk@live.com>
2023-06-01 20:59:46 -0400

    ajuste a ingresa.php

--- ingresa1.php
+++ ingresa1.php
@@ -7,6 +7,8 @@ $nombres=$_POST['Nombres'];
 $apellidos=$_POST['Apellidos'];
 $usuario=$_POST['Nombre_us'];
 $password=$_POST['Password'];
+$pre=$_POST['Pre'];
+$res=$_POST['Res'];
 $email=$_POST['Email'];
 $fono=$_POST['Fono'];
 $direccion=$_POST['Direccion'];
@@ -16,7 +18,7 @@ $marca=$_POST['Marca_fav'];
 $adm=$_POST['adm'];
 
 
-$sql="INSERT INTO usuarios1 VALUES('$num','$nombres','$apellidos','$usuario','$password','$email','$fono','$direccion','$fecha','$sexo','$marca','$adm')";
+$sql="INSERT INTO usuarios1 VALUES('$num','$nombres','$apellidos','$usuario','$password','$pre','$res','$email','$fono','$direccion','$fecha','$sexo','$marca','$adm')";
 $query= mysqli_query($conexion,$sql);
 
 if($query){
